4f6827f5dae83cf2a23923ba3555a0a3fa91aac4
Danvintius <danila.94@mail.ru>
2023-05-07 21:53:47 +0300

    Оформление

--- main.js
+++ main.js
@@ -9,13 +9,26 @@ let message = document.querySelector('.subscription-message');
 let form = document.querySelector('.subscription');
 let email = document.querySelector('.subscription-email');
 
-form.onsubmit = function(evt) {
-  evt.preventDefault();
+// form.onsubmit = function(evt) {
+//  evt.preventDefault();
   // Измените значение textContent на следующей строке
-  message.textContent = 'Send to ' + email.value;
-};
+//  message.textContent = 'Send to ' + email.value;
+//};
 
 let news1 = document.querySelector('.news1');
 function News() {
 document.write("Первая новость");
+}
+
+const btn = document.querySelector('.free-fragment');
+const counter = document.querySelector('.counter')
+btn.onclick = function() {
+  console.log("Нажали");
+  window.alert('Бесплатный фрагмент');
+  counter.textContent++;
+}
+
+const btnBought = document.querySelector('.bought')
+btnBought.onclick = function() {
+  document.location = 'Об_авторе.htm'
 }
